3ee47d32027a80ddd59e6f0281f9a866f6e745b2
kruzhkaKofe <pouncevokno8@gmail.com>
2022-07-12 11:18:02 +0400

    create BaseLayout

---
+++ src/components/base/BaseLayout.vue
@@ -0,0 +1,21 @@
+<template>
+	<ion-page>
+		<base-main-menu content-id="main"></base-main-menu>
+		<base-header id="main"></base-header>
+		<ion-content>
+			<slot></slot>
+			<base-footer></base-footer>
+		</ion-content>
+	</ion-page>
+</template>
+
+<script setup>
+import {IonPage, IonContent} from '@ionic/vue'
+import BaseHeader from "@/components/base/BaseHeader"
+import BaseFooter from "@/components/base/BaseFooter"
+import BaseMainMenu from "@/components/base/BaseMainMenu.vue";
+</script>
+
+<style lang="sass" scoped>
+
+</style>
