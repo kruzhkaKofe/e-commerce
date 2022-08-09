8b4e4f963e607be2d78efe7a80492c513bea75ce
kruzhkaKofe <pouncevokno8@gmail.com>
2022-08-09 15:54:31 +0400

    add BaseBlackButtonSmall

---
+++ src/components/base/BaseBlackButtonSmall.vue
@@ -0,0 +1,23 @@
+<template>
+  <ion-button class="button">
+		<slot></slot>
+	</ion-button>
+</template>
+
+<script setup>
+import { IonButton } from "@ionic/vue";
+</script>
+
+<style lang="sass" scoped>
+.button
+	backdrop-filter: blur(4px)
+	font-size: 16px
+	line-height: 24px
+	--padding-top: 8px
+	--padding-bottom: 8px
+	--padding-start: 30px
+	--padding-end: 30px
+	--background: #000000
+	--border-radius: 0
+	--color: #FCFCFC
+</style>
